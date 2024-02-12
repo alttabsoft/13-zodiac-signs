4f0c490de0cab501554cc53d946914193501bc3e
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-02-13 04:09:09 +0900

    Feat: 기본 구조 구성

---
+++ Assets/13-zodiac-sign/Scripts/UIScripts/TempUI.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _13_zodiac_sign.Scripts.UIScripts
+{
+    public class TempUI : MonoBehaviour
+    {
+        private void Start()
+        {
+            var buttons = GetComponentsInChildren<Button>(includeInactive: true); // 버튼별 역할 할당,
+            foreach (var button in buttons)
+            {
+                var buttonName = button.transform.name;
+                if (buttonName == "BattleSceneButton")
+                {
+                    button.onClick.AddListener(() =>
+                    {
+                        SceneManager.LoadScene("BattleScene");
+                    });
+                }
+                if (buttonName == "CubeSceneButton")
+                {
+                    button.onClick.AddListener(() =>
+                    {
+                        SceneManager.LoadScene("CubeScene");
+                    });
+                }
+                
+                if (buttonName == "MainMenuSceneButton")
+                {
+                    button.onClick.AddListener(() =>
+                    {
+                        SceneManager.LoadScene("MainMenuScene");
+                    });
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
